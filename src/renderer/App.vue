bd0e40977ae7fc8b0c4be4556ac82d1afa559b6e
逐云 <dfy30866@qq.com>
2017-12-29 09:42:35 +0800

    初始化

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'toolbox'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
